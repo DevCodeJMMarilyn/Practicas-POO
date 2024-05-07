a27a51bddee0b8a3ecbe1e5d8db17a333f14cfe1
Marilyn <140780754+DevCodeJMMarilyn@users.noreply.github.com>
2024-05-07 12:02:12 -0600

    Add files via upload

---
+++ practica13/main.js
@@ -0,0 +1,33 @@
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.resend.com",
+  port: 465,
+  secure: true, // Use `true` for port 465, `false` for all other ports
+  auth: {
+    user: "resend",
+    pass: "re_HXnLpknM_Kfrbu9LpNnDFnHC7aASLtpbE",
+  },
+});
+
+// async..await is not allowed in global scope, must use a wrapper
+async function main() {
+  // send mail with defined transport object
+  const info = await transporter.sendMail({
+    from: '"Marilyn Jimenez 👻" <onboarding@resend.dev>', // sender address
+    to: "jimenezm.m2021@gmail.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
+}
+
+main().catch(console.error);
+
+//aplicando poo
+class mensaje{
+    
+}
